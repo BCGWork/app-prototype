2a5557b509359b33a650229f66811a864a1b499e
Boxuan Cui <boxuancui@gmail.com>
2014-10-06 20:27:48 -0400

    added date video dimension

--- youtube_api/analytics_video.py
+++ youtube_api/analytics_video.py
@@ -23,15 +23,15 @@ from oauth2client.tools import argparser, run_flow
 # Please ensure that you have enabled the YouTube Data and YouTube Analytics
 # APIs for your project.
 # For more information about using OAuth2 to access the YouTube Data API, see:
-#   https://developers.google.com/youtube/v3/guides/authentication
+#	 https://developers.google.com/youtube/v3/guides/authentication
 # For more information about the client_secrets.json file format, see:
-#   https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
+#	 https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
 CLIENT_SECRETS_FILE = "client_secrets.json"
 
 # These OAuth 2.0 access scopes allow for read-only access to the authenticated
 # user's account for both YouTube Data API resources and YouTube Analytics Data.
 YOUTUBE_SCOPES = ["https://www.googleapis.com/auth/youtube.readonly",
-  "https://www.googleapis.com/auth/yt-analytics.readonly"]
+	"https://www.googleapis.com/auth/yt-analytics.readonly"]
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 YOUTUBE_ANALYTICS_API_SERVICE_NAME = "youtubeAnalytics"
@@ -45,105 +45,126 @@ WARNING: Please configure OAuth 2.0
 To make this sample run you will need to populate the client_secrets.json file
 found at:
 
-   %s
+	 %s
 
 with information from the Developers Console
 https://console.developers.google.com/
 
 For more information about the client_secrets.json file format, please visit:
 https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
-""" % os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                   CLIENT_SECRETS_FILE))
+""" % os.path.abspath(os.path.join(os.path.dirname(__file__), CLIENT_SECRETS_FILE))
 
 
 def get_authenticated_services(args):
-  flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE,
-    scope=" ".join(YOUTUBE_SCOPES),
-    message=MISSING_CLIENT_SECRETS_MESSAGE)
+	flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE,
+		scope=" ".join(YOUTUBE_SCOPES),
+		message=MISSING_CLIENT_SECRETS_MESSAGE)
 
-  storage = Storage("%s-oauth2.json" % sys.argv[0])
-  credentials = storage.get()
+	storage = Storage("%s-oauth2.json" % sys.argv[0])
+	credentials = storage.get()
 
-  if credentials is None or credentials.invalid:
-    credentials = run_flow(flow, storage, args)
+	if credentials is None or credentials.invalid:
+		credentials = run_flow(flow, storage, args)
 
-  http = credentials.authorize(httplib2.Http())
+	http = credentials.authorize(httplib2.Http())
+	youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, http=http)
+	youtube_analytics = build(YOUTUBE_ANALYTICS_API_SERVICE_NAME, YOUTUBE_ANALYTICS_API_VERSION, http=http)
 
-  youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
-    http=http)
-  youtube_analytics = build(YOUTUBE_ANALYTICS_API_SERVICE_NAME,
-    YOUTUBE_ANALYTICS_API_VERSION, http=http)
-
-  return (youtube, youtube_analytics)
+	return (youtube, youtube_analytics)
 
 def get_channel_id(youtube):
-  channels_list_response = youtube.channels().list(
-    mine=True,
-    part="id"
-  ).execute()
+	channels_list_response = youtube.channels().list(
+		mine=True,
+		part="id"
+	).execute()
 
-  return channels_list_response["items"][0]["id"]
+	return channels_list_response["items"][0]["id"]
 
 def run_analytics_report(youtube_analytics, channel_id, options):
-  # Call the Analytics API to retrieve a report. For a list of available
-  # reports, see:
-  # https://developers.google.com/youtube/analytics/v1/channel_reports
-  analytics_query_response = youtube_analytics.reports().query(
-    ids="channel==%s" % channel_id,
-    metrics=options.metrics,
-    dimensions=options.dimensions,
-    start_date=options.start_date,
-    end_date=options.end_date,
-    max_results=options.max_results,
-	filters=options.filters
-    # sort=options.sort
-  ).execute()
+	# Call the Analytics API to retrieve a report. For a list of available
+	# reports, see:
+	# https://developers.google.com/youtube/analytics/v1/channel_reports
+	analytics_query_response = youtube_analytics.reports().query(
+		ids="channel==%s" % channel_id,
+		metrics=options.metrics,
+		dimensions=options.dimensions,
+		start_date=options.start_date,
+		end_date=options.end_date,
+		# max_results=options.max_results,
+		# sort=options.sort,
+		filters=options.filters
+	).execute()
 
-  return analytics_query_response.get("rows", [])
+	return analytics_query_response.get("rows", [])
 
 
 if __name__ == "__main__":
-  now = datetime.now()
-  one_day_ago = (now - timedelta(days=1)).strftime("%Y-%m-%d")
-  one_week_ago = (now - timedelta(days=7)).strftime("%Y-%m-%d")
+	# now = datetime.now()
+	# one_day_ago = (now - timedelta(days=1)).strftime("%Y-%m-%d")
+	# one_week_ago = (now - timedelta(days=7)).strftime("%Y-%m-%d")
+	startDate = "2009-06-23"
+	endDate = "2014-10-04"
 
-  argparser.add_argument("--metrics", help="Report metrics",
-    default="views,comments,favoritesAdded,favoritesRemoved,likes,dislikes,shares,estimatedMinutesWatched,averageViewDuration,averageViewPercentage,subscribersGained,subscribersLost")
-  argparser.add_argument("--dimensions", help="Report dimensions",
-    default="video")
-  argparser.add_argument("--start-date", default=one_week_ago,
-    help="Start date, in YYYY-MM-DD format")
-  argparser.add_argument("--end-date", default=one_day_ago,
-    help="End date, in YYYY-MM-DD format")
-  argparser.add_argument("--max-results", help="Max results", default=10)
-  argparser.add_argument("--filters", default="video==GNZBSZD16cY")
-  # argparser.add_argument("--sort", help="Sort order", default="-views")
-  args = argparser.parse_args()
+	argparser.add_argument("--metrics", help="Report metrics",
+		default="views,comments,favoritesAdded,favoritesRemoved,likes,dislikes,shares,estimatedMinutesWatched,averageViewDuration,averageViewPercentage,subscribersGained,subscribersLost")
+	argparser.add_argument("--dimensions", help="Report dimensions", default="video")
+	argparser.add_argument("--start-date", default=startDate, help="Start date, in YYYY-MM-DD format")
+	argparser.add_argument("--end-date", default=endDate, help="End date, in YYYY-MM-DD format")
+	# argparser.add_argument("--max-results", help="Max results", default=10)
+	argparser.add_argument("--filters", default="video==GNZBSZD16cY")
+	# argparser.add_argument("--sort", help="Sort order", default="-views")
+	args = argparser.parse_args()
+	(youtube, youtube_analytics) = get_authenticated_services(args)
+	
+	''' METRICS FOR DATE AND VIDEO DIMENSION '''
+	vFile = csv.reader(open("video_data.csv", "r"), delimiter = ",")
+	vFile.next()
+	vDict = {}
+	for row in vFile:
+		vDict[row[0]] = row[2][0:10]
+	vData = csv.writer(open("analytics_date_video.csv", "wb+"))
+	vData.writerow(["video_id", "date", "views", "comments", "favoritesAdded", "favoritesRemoved", "likes", "dislikes", "shares", "estimatedMinutesWatched", "averageViewDuration", "averageViewPercentage", "subscribersGained", "subscribersLost"])
+	
+	try:
+		channel_id = get_channel_id(youtube)
+		start_time = time.time()
+		for item in vDict.keys():
+			args.dimensions = "day"
+			args.start_date = vDict[item]
+			args.filters = "video==" + item
+			vDataRow = run_analytics_report(youtube_analytics, channel_id, args)
+			for row in vDataRow:
+				row.insert(0, item)
+				vData.writerow(row)
+			print "Data recorded for video " + item
+		elapsed_time = time.time() - start_time
+		print "Total elapsed time: " + str(elapsed_time) + " seconds"
+	except HttpError, e:
+		print "An HTTP error %d occurred:\n%s" % (e.resp.status, e.content)
 
-  (youtube, youtube_analytics) = get_authenticated_services(args)
-  
-  vFile = csv.reader(open("video_data.csv", "r"), delimiter = ",")
-  vFile.next()
-  vList = []
-  for row in vFile:
-    vList.append(row[0])
 
-  vData = csv.writer(open("analytics_video_data_3.csv", "wb+"))
-  vData.writerow(["video_id", "views", "comments", "favoritesAdded", "favoritesRemoved", "likes", "dislikes", "shares", "estimatedMinutesWatched", "averageViewDuration", "averageViewPercentage", "subscribersGained", "subscribersLost"])
+	''' METRICS FOR VIDEO DIMENSION ONLY '''
+	# vFile = csv.reader(open("video_data.csv", "r"), delimiter = ",")
+	# vFile.next()
+	# vList = []
+	# for row in vFile:
+		# vList.append(row[0])
+	# vData = csv.writer(open("analytics_video_data.csv", "wb+"))
+	# vData.writerow(["video_id", "views", "comments", "favoritesAdded", "favoritesRemoved", "likes", "dislikes", "shares", "estimatedMinutesWatched", "averageViewDuration", "averageViewPercentage", "subscribersGained", "subscribersLost"])
 
-  try:
-    iter = len(vList)/10
-    channel_id = get_channel_id(youtube)
-    start_time = time.time()
-    for j in range(1443, iter):
-      args.filters = "video==" + ",".join(vList[(10*j+1):(10*j+11)])
-      vDataRow = run_analytics_report(youtube_analytics, channel_id, args)
-      for row in vDataRow:
-        vData.writerow(row)
-      print "Data for group " + str(j) + " recorded"
-    elapsed_time = time.time() - start_time
-    print "Total elapsed time: " + str(elapsed_time) + " seconds"
-  except HttpError, e:
-    print "An HTTP error %d occurred:\n%s" % (e.resp.status, e.content)
+	# try:
+		# iter = len(vList)/10
+		# channel_id = get_channel_id(youtube)
+		# start_time = time.time()
+		# for j in range(iter):
+			# args.filters = "video==" + ",".join(vList[(10*j+1):(10*j+11)])
+			# vDataRow = run_analytics_report(youtube_analytics, channel_id, args)
+			# for row in vDataRow:
+				# vData.writerow(row)
+			# print "Data for group " + str(j) + " recorded"
+		# elapsed_time = time.time() - start_time
+		# print "Total elapsed time: " + str(elapsed_time) + " seconds"
+	# except HttpError, e:
+		# print "An HTTP error %d occurred:\n%s" % (e.resp.status, e.content)
 
 
