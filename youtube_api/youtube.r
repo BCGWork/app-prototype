93b5e382b61e443899a102d0292bb47851ba0cb7
boc <cui.bo@bcg.com>
2014-10-08 15:28:32 -0400

    Added r script for merging video by date data

--- youtube_api/youtube.r
+++ youtube_api/youtube.r
@@ -12,13 +12,24 @@ data1 <- fread("../analytics_date_video_1.csv", header=TRUE, sep=",")
 data2 <- fread("../analytics_date_video_2.csv", header=TRUE, sep=",")
 data3 <- fread("../analytics_date_video_3.csv", header=TRUE, sep=",")
 data4 <- fread("../analytics_date_video_4.csv", header=TRUE, sep=",")
-existVideo <- c(
-  unique(data1$video_id),
-  unique(data2$video_id),
-  unique(data3$video_id),
-  unique(data4$video_id)
-)
-write.csv(existVideo, file="existing_videos.csv", row.names=FALSE)
+data5 <- fread("../analytics_date_video_5.csv", header=TRUE, sep=",")
+data6 <- fread("../analytics_date_video_6.csv", header=TRUE, sep=",")
+
+# existVideo <- c(
+#   unique(data1$video_id),
+#   unique(data2$video_id),
+#   unique(data3$video_id),
+#   unique(data4$video_id),
+#   unique(data5$video_id),
+#   unique(data6$video_id)
+# )
+# write.csv(existVideo, file="existing_videos.csv", row.names=FALSE)
+
+data <- do.call(rbind, list(data1, data2, data3, data4, data5, data6))
+data$date <- as.Date(data$date)
+save(data, file="metrics_by_video_date.RData")
+write.csv(data, file="analytics_date_video.csv", row.names=FALSE)
+
 
 
 
